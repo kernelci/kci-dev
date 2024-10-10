dbfa27c1b08c100648f173144926abb5a92ced34
Denys Fedoryshchenko <denys.f@collabora.com>
2024-10-10 14:38:29 +0300

    feat(checkout): Add --watch option
    Watch interactively results of custom checkout, each step,
    until job is completed.
    Example:
    ./kci-dev.py checkout --giturl https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git --branch master --tipoftree --jobfilter baseline-nfs-arm64-qualcomm --jobfilter kbuild-gcc-12-arm64-chromeos-qualcomm --watch
    
    Signed-off-by: Denys Fedoryshchenko <denys.f@collabora.com>

--- kci-dev/subcommands/checkout.py
+++ kci-dev/subcommands/checkout.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import datetime
 import json
 import re
 import subprocess
+import time
 
 import click
 import requests
@@ -22,6 +24,8 @@ def display_api_error(response):
         click.secho(response.json(), fg="red")
     except json.decoder.JSONDecodeError:
         click.secho(f"No JSON response. Plain text: {response.text}", fg="yellow")
+    except Exception as e:
+        click.secho(f"API response error: {e}: {response.text}", fg="red")
     return
 
 
@@ -39,7 +43,7 @@ def send_checkout_full(baseurl, token, **kwargs):
     }
     jdata = json.dumps(data)
     try:
-        response = requests.post(url, headers=headers, data=jdata)
+        response = requests.post(url, headers=headers, data=jdata, timeout=30)
     except requests.exceptions.RequestException as e:
         click.secho(f"API connection error: {e}", fg="red")
         return
@@ -48,6 +52,98 @@ def send_checkout_full(baseurl, token, **kwargs):
         display_api_error(response)
         return None
     return response.json()
+
+
+def retrieve_treeid_nodes(baseurl, token, treeid):
+    url = baseurl + "latest/nodes/fast?treeid=" + treeid
+    headers = {
+        "Content-Type": "application/json; charset=utf-8",
+        "Authorization": f"{token}",
+    }
+    try:
+        response = requests.get(url, headers=headers, timeout=30)
+    except requests.exceptions.RequestException as e:
+        click.secho(f"API connection error: {e}, retrying...", fg="yellow")
+        return None
+    except Exception as e:
+        click.secho(f"API connection error: {e}, retrying...", fg="yellow")
+        return None
+
+    if response.status_code >= 400:
+        display_api_error(response)
+        return None
+
+    return response.json()
+
+
+def check_node(node):
+    """
+    Node can be defined RUNNING/DONE/FAIL based on the state
+    Simplify, as our current state model suboptimal
+    """
+    name = node["name"]
+    state = node["state"]
+    result = node["result"]
+    if name == "checkout":
+        if state == "running":
+            return "RUNNING"
+        elif state == "available" or state == "closing":
+            return "DONE"
+        elif state == "done" and result == "pass":
+            return "DONE"
+        else:
+            return "FAIL"
+    else:
+        if state == "running":
+            return "RUNNING"
+        elif state == "done" and result == "pass":
+            return "DONE"
+        else:
+            return "FAIL"
+
+
+def watch_jobs(baseurl, token, treeid, jobfilter):
+    # we need to add to jobfilter "checkout" node
+    jobfilter = list(jobfilter)
+    jobfilter.append("checkout")
+    while True:
+        inprogress = 0
+        joblist = jobfilter.copy()
+        nodes = retrieve_treeid_nodes(baseurl, token, treeid)
+        if not nodes:
+            click.secho("No nodes found. Retrying...", fg="yellow")
+            time.sleep(5)
+            continue
+        time_local = time.localtime()
+        click.echo(f"Current time: {time.strftime('%Y-%m-%d %H:%M:%S', time_local)}")
+        click.secho(f"Total tree nodes {len(nodes)} found.", fg="green")
+
+        # Tricky part in watch is that we might have one item in jobfilter (job, test),
+        # but it might spawn multiple nodes with same name
+        for node in nodes:
+            if node["name"] in jobfilter:
+                result = check_node(node)
+                if result == "DONE":
+                    if isinstance(joblist, list) and node["name"] in joblist:
+                        joblist.remove(node["name"])
+                    color = "green"
+                elif result == "RUNNING":
+                    inprogress += 1
+                    color = "yellow"
+                else:
+                    if isinstance(joblist, list) and node["name"] in joblist:
+                        joblist.remove(node["name"])
+                    color = "red"
+                click.secho(
+                    f"Node {node['_id']} job {node['name']} State {node['state']} Result {node['result']}",
+                    fg=color,
+                )
+        if len(joblist) == 0 and inprogress == 0:
+            click.secho("All jobs completed", fg="green")
+            return
+
+        click.echo(f"\rRefresh in 30s...", nl=False)
+        time.sleep(30)
 
 
 def retrieve_tot_commit(repourl, branch):
@@ -85,6 +181,11 @@ def retrieve_tot_commit(repourl, branch):
     help="Checkout on latest commit on tree/branch",
     is_flag=True,
 )
+@click.option(
+    "--watch",
+    help="Interactively watch for a tasks in jobfilter",
+    is_flag=True,
+)
 # jobfilter is a list, might be one or more jobs
 @click.option(
     "--jobfilter",
@@ -92,14 +193,18 @@ def retrieve_tot_commit(repourl, branch):
     multiple=True,
 )
 @click.pass_context
-def checkout(ctx, giturl, branch, commit, jobfilter, tipoftree):
+def checkout(ctx, giturl, branch, commit, jobfilter, tipoftree, watch):
     cfg = ctx.obj.get("CFG")
     instance = ctx.obj.get("INSTANCE")
     url = api_connection(cfg[instance]["pipeline"])
+    apiurl = cfg[instance]["api"]
     token = cfg[instance]["token"]
     if not jobfilter:
         jobfilter = None
         click.secho("No job filter defined. All jobs will be triggered!", fg="yellow")
+    if watch and not jobfilter:
+        click.secho("No job filter defined. Can't watch for a job(s)!", fg="red")
+        return
     if not commit and not tipoftree:
         click.secho("No commit or tree/branch latest commit defined", fg="red")
         return
@@ -110,10 +215,26 @@ def checkout(ctx, giturl, branch, commit, jobfilter, tipoftree):
         commit = retrieve_tot_commit(giturl, branch)
         click.secho(f"Commit to checkout: {commit}", fg="green")
     resp = send_checkout_full(
-        url, token, giturl=giturl, branch=branch, commit=commit, jobfilter=jobfilter
+        url,
+        token,
+        giturl=giturl,
+        branch=branch,
+        commit=commit,
+        jobfilter=jobfilter,
+        watch=watch,
     )
     if resp and "message" in resp:
         click.secho(resp["message"], fg="green")
+
+    if watch and isinstance(resp, dict):
+        node = resp.get("node")
+        treeid = node.get("treeid")
+        if not treeid:
+            click.secho("No treeid returned. Can't watch for a job(s)!", fg="red")
+            return
+        click.secho(f"Watching for jobs on treeid: {treeid}", fg="green")
+        # watch for jobs
+        watch_jobs(apiurl, token, treeid, jobfilter)
 
 
 if __name__ == "__main__":
