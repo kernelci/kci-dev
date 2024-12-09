721542cae3577dc1df2cdaf9c4b02d7ddaff0dd7
Gustavo Padovan <gustavo.padovan@collabora.com>
2024-12-09 15:56:45 -0300

    cmds: add experimental results fetch from Dashboard API
    It introduces `kci-dev results` to supporting pulling results
    data in various forms from the Dashboard API.
    
    For now it has two actions (through the --action param)
    
    1. `--action=summary`: to get a basic numeric summary of pass/fail
    
    2. `--action=failed-builds`: to get the list of fail builds
    
    Addionally it has the `--download-logs` option to download the
    build logs with errors.
    
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.com>

--- kcidev/main.py
+++ kcidev/main.py
@@ -4,7 +4,14 @@
 import click
 
 from kcidev.libs.common import *
-from kcidev.subcommands import checkout, commit, patch, maestro_results, testretry
+from kcidev.subcommands import (
+    checkout,
+    commit,
+    maestro_results,
+    patch,
+    results,
+    testretry,
+)
 
 
 @click.group(
@@ -20,18 +27,21 @@ from kcidev.subcommands import checkout, commit, patch, maestro_results, testret
 @click.option("--instance", help="API instance to use", required=False)
 @click.pass_context
 def cli(ctx, settings, instance):
-    ctx.obj = {"CFG": load_toml(settings)}
-    if instance:
-        ctx.obj["INSTANCE"] = instance
-    else:
-        ctx.obj["INSTANCE"] = ctx.obj["CFG"].get("default_instance")
-    if not ctx.obj["INSTANCE"]:
-        click.secho("No instance defined in settings or as argument", fg="red")
-        raise click.Abort()
-    if ctx.obj["INSTANCE"] not in ctx.obj["CFG"]:
-        click.secho(f"Instance {ctx.obj['INSTANCE']} not found in {settings}", fg="red")
-        raise click.Abort()
-    pass
+    if ctx.invoked_subcommand != "results":
+        ctx.obj = {"CFG": load_toml(settings)}
+        if instance:
+            ctx.obj["INSTANCE"] = instance
+        else:
+            ctx.obj["INSTANCE"] = ctx.obj["CFG"].get("default_instance")
+        if not ctx.obj["INSTANCE"]:
+            click.secho("No instance defined in settings or as argument", fg="red")
+            raise click.Abort()
+        if ctx.obj["INSTANCE"] not in ctx.obj["CFG"]:
+            click.secho(
+                f"Instance {ctx.obj['INSTANCE']} not found in {settings}", fg="red"
+            )
+            raise click.Abort()
+        pass
 
 
 def run():
@@ -40,6 +50,7 @@ def run():
     cli.add_command(patch.patch)
     cli.add_command(maestro_results.maestro_results)
     cli.add_command(testretry.testretry)
+    cli.add_command(results.results)
     cli()
 
 
