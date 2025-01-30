6ac7cf0e4f67bb3187c4f709bb054cc8cf829f58
Gustavo Padovan <gustavo.padovan@collabora.com>
2025-01-30 14:59:44 -0300

    maestro_results: make get_nodes functions common to other cmds
    We will want to use the infra to get nodes from other commands.
    
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.com>

--- kcidev/libs/maestro_common.py
+++ kcidev/libs/maestro_common.py
@@ -4,6 +4,7 @@
 import json
 
 import click
+import requests
 
 from kcidev.libs.common import *
 
@@ -23,3 +24,62 @@ def maestro_api_error(response):
     except Exception as e:
         kci_err(f"API response error: {e}: {response.text}")
     return
+
+
+def maestro_print_nodes(nodes, field):
+    res = []
+    if not isinstance(nodes, list):
+        nodes = [nodes]
+    for node in nodes:
+        if field:
+            data = {}
+            for f in field:
+                data[f] = node.get(f)
+            res.append(data)
+        else:
+            res.append(node)
+        kci_msg(json.dumps(res, sort_keys=True, indent=4))
+
+
+def maestro_get_node(url, nodeid):
+    headers = {
+        "Content-Type": "application/json; charset=utf-8",
+    }
+    url = url + "latest/node/" + nodeid
+    maestro_print_api_call(url)
+    response = requests.get(url, headers=headers)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as ex:
+        kci_err(ex.response.json().get("detail"))
+        return None
+    except Exception as ex:
+        kci_err(ex)
+        return None
+
+    return response.json()
+
+
+def maestro_get_nodes(url, limit, offset, filter):
+    headers = {
+        "Content-Type": "application/json; charset=utf-8",
+    }
+    url = url + "latest/nodes/fast?limit=" + str(limit) + "&offset=" + str(offset)
+    maestro_print_api_call(url)
+    if filter:
+        for f in filter:
+            # TBD: We need to add translate filter to API
+            # if we need anything more complex than eq(=)
+            url = url + "&" + f
+
+    response = requests.get(url, headers=headers)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as ex:
+        kci_err(ex.response.json().get("detail"))
+        return None
+    except Exception as ex:
+        kci_err(ex)
+        return None
+
+    return response.json()
--- kcidev/subcommands/maestro_results.py
+++ kcidev/subcommands/maestro_results.py
@@ -1,74 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import json
-import pprint
-
 import click
-import requests
 from git import Repo
 
 from kcidev.libs.common import *
 from kcidev.libs.maestro_common import *
-
-
-def print_nodes(nodes, field):
-    res = []
-    if not isinstance(nodes, list):
-        nodes = [nodes]
-    for node in nodes:
-        if field:
-            data = {}
-            for f in field:
-                data[f] = node.get(f)
-            res.append(data)
-        else:
-            res.append(node)
-        kci_msg(json.dumps(res, sort_keys=True, indent=4))
-
-
-def get_node(url, nodeid, field):
-    headers = {
-        "Content-Type": "application/json; charset=utf-8",
-    }
-    url = url + "latest/node/" + nodeid
-    maestro_print_api_call(url)
-    response = requests.get(url, headers=headers)
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as ex:
-        kci_err(ex.response.json().get("detail"))
-        return None
-    except Exception as ex:
-        kci_err(ex)
-        return None
-    print_nodes(response.json(), field)
-
-
-def get_nodes(url, limit, offset, filter, field):
-    headers = {
-        "Content-Type": "application/json; charset=utf-8",
-    }
-    url = url + "latest/nodes/fast?limit=" + str(limit) + "&offset=" + str(offset)
-    maestro_print_api_call(url)
-    if filter:
-        for f in filter:
-            # TBD: We need to add translate filter to API
-            # if we need anything more complex than eq(=)
-            url = url + "&" + f
-
-    response = requests.get(url, headers=headers)
-    try:
-        response.raise_for_status()
-    except requests.exceptions.HTTPError as ex:
-        kci_err(ex.response.json().get("detail"))
-        return None
-    except Exception as ex:
-        kci_err(ex)
-        return None
-
-    nodes = response.json()
-    print_nodes(nodes, field)
 
 
 @click.command(help="Get results directly from KernelCI's Maestro")
@@ -113,9 +50,10 @@ def maestro_results(ctx, nodeid, nodes, limit, offset, filter, field):
     instance = ctx.obj.get("INSTANCE")
     url = config[instance]["api"]
     if nodeid:
-        get_node(url, nodeid, field)
+        results = maestro_get_node(url, nodeid)
     if nodes:
-        get_nodes(url, limit, offset, filter, field)
+        results = maestro_get_nodes(url, limit, offset, filter)
+    maestro_print_nodes(results, field)
 
 
 if __name__ == "__main__":
