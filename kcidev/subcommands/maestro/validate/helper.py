d7898de100edd89c3be5f05464c2a271c678c360
Jeny Sadadia <jeny.sadadia@collabora.com>
2026-01-07 13:23:18 +0530

    Remove unnecessary print while getting builds history
    Signed-off-by: Jeny Sadadia <jeny.sadadia@collabora.com>

--- kcidev/subcommands/maestro/validate/helper.py
+++ kcidev/subcommands/maestro/validate/helper.py
@@ -368,10 +368,10 @@ def get_checkouts(ctx, giturl, branch, days):
     ]
     checkouts = ctx.invoke(
         results,
-        count=True,
         nodes=True,
         filter=filters,
         paginate=False,
+        verbose=False,
     )
     return checkouts
 
@@ -391,10 +391,10 @@ def get_builds_history(ctx, checkouts, arch):
             filters.append(f"data.arch={arch}")
         maestro_builds = ctx.invoke(
             results,
-            count=True,
             nodes=True,
             filter=filters,
             paginate=False,
+            verbose=False,
         )
 
         branch = c["data"]["kernel_revision"].get("branch")
